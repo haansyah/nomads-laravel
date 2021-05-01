3f3920fd070c1476e884b2ae618e861f6f5c77a0
Ari Kun <61754538+haansyah@users.noreply.github.com>
2021-05-01 07:13:59 +0700

    Memisahkan Layout dan Kontent

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Admin\DashboardController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->group( function() {
+        Route::get('/', [DashboardController::class, 'index'])
+            ->name('dashboard');
 });
